a87278b6119665c7b8e783981d18262d0810094a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-09 19:32:05 +0530

    largest num using ternary

--- Largest.java
+++ Largest.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 
 public class Largest {
     public static void main(String[] args) {
-    Scanner input=new Scanner(System.in);
-    
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter three numbers: ");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int num3 = input.nextInt();
+        System.out.println("largest num: " + (num1 > num2 ? num1 > num3 ? num1 : num3 : num2));
+
     }
 }
