68faa2da88116f48b7b00ed5671e919ff4b21006
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-09 20:00:18 +0530

    fibonacci

--- fibonacci.java
+++ fibonacci.java
@@ -5,10 +5,11 @@ public class fibonacci {
         System.out.print(a + " " + b + " ");
         for (int i = 3; i <= n; i++) {
             int c = a + b;
+            System.out.print(c + " ");
             a = b;
             b = c;
-            System.out.print(c + " ");
         }
+       
     }
 
     public static void main(String[] args) {
