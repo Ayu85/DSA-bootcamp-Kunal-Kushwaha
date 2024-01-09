219b6d72e77d6e5ef10a9bd08606e239f13ecac8
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-09 20:05:20 +0530

    nth fibonacci and get n fibonacci

--- fibonacci.java
+++ fibonacci.java
@@ -2,17 +2,18 @@ public class fibonacci {
     static void printFib(int n) {
         int a = 0;
         int b = 1;
-        System.out.print(a + " " + b + " ");
+        // System.out.print(a + " " + b + " ");
         for (int i = 3; i <= n; i++) {
             int c = a + b;
-            System.out.print(c + " ");
+            // System.out.print(c + " ");
             a = b;
             b = c;
         }
+        System.out.println(b);
        
     }
 
     public static void main(String[] args) {
-        printFib(10);
+        printFib(6);
     }
 }
