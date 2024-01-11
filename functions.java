a7e372acf2192451cc5712eb2e940622dec2313f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-11 18:59:07 +0530

    functions

---
+++ functions.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class functions {
+    static int getSUm(int a,int b){
+        return a+b;
+    }
+
+    static float getSUm(float a,float b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        System.out.println(getSUm(2.5f,30.5f));
+    }
+}
