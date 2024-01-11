8949aa23283f939fa59cc03bc717e4454aa84b9a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-11 19:33:39 +0530

    armstrong number

---
+++ armstrong.java
@@ -0,0 +1,21 @@
+public class armstrong {
+    static boolean isArms(int n){
+
+       int org=n;
+        int result=0;
+        while(n>0){
+            int rem=n%10;
+            result+=rem*rem*rem;
+            n/=10;
+        }
+        if(result==org)
+            return true;
+        else
+            return false;
+
+
+    }
+    public static void main(String[] args) {
+        System.out.println(isArms(153));
+    }
+}
