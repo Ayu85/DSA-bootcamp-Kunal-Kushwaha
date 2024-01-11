07df87a2858c8d67c14956aa9ddf0dd2d01f7a9b
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-11 18:38:49 +0530

    calculator

--- calculator.java
+++ calculator.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class calculator {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter The Operation : ");
+        char ch=sc.next().charAt(0);
+
+
     }
 }
