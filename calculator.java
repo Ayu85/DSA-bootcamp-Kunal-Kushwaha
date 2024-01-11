2dc136e708365a9f4f530112f1bd923c3751c5f4
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-11 18:51:10 +0530

    calculator

--- calculator.java
+++ calculator.java
@@ -2,9 +2,26 @@ import java.util.Scanner;
 public class calculator {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter The Operation : ");
+        System.out.println("Enter The Operation (+,-,/,*): ");
         char ch=sc.next().charAt(0);
-
-
+        System.out.println("Enter Two Numbers: ");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        switch (ch){
+            case '+':
+                System.out.println(a+b);
+                break;
+            case '-':
+                System.out.println(a-b);
+                break;
+            case '/':
+                System.out.println(a/b);
+                break;
+            case '*':
+                System.out.println(a*b);
+                break;
+            default:
+                System.out.println("Wrong input ");
+        }
     }
 }
