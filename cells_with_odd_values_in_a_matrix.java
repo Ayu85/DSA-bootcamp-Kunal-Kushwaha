677e6c35fceaaeeca69b0c5d154c8eac7906315f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-14 19:00:14 +0530

    cells_with_odd_values_in_a_matrix

--- cells_with_odd_values_in_a_matrix.java
+++ cells_with_odd_values_in_a_matrix.java
@@ -1,8 +1,37 @@
 public class cells_with_odd_values_in_a_matrix {
-    static  void oddCells(int[][] indices,int m,int n){
-
+    static  int[][] oddCells(int[][] indices,int m,int n){
+            int[][] temp=new int[m][n];
+            for(int i=0;i<indices.length;i++){
+                boolean isRow=true;
+                for(int j=0;j<indices[i].length;j++){
+                    int index=indices[i][j];
+                    if(isRow){
+                        int y=0;
+                        for(int x=0;x<temp[index].length;x++){
+                            temp[index][x]+=1;
+                            isRow=false;
+                            y++;
+                        }
+                    }
+                    else {
+                        for(int o=0;o<temp.length;o++){
+                            int p=0;
+                            temp[o][index]+=1;
+                            isRow=true;
+                        }
+                    }
+                }
+            }
+            return temp;
     }
     public static void main(String[] args) {
-
+        int[][] ind={{0,1},{1,1}};
+    int[][] res=oddCells(ind,2,3);
+    for(int i=0;i<res.length;i++){
+       for(int j=0;j<res[i].length;j++){
+           System.out.print(res[i][j]+" ");
+       }
+       System.out.println();
+   }
     }
 }
