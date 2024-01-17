39657cc737c79fbc8c0d223fe2002302024d40de
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-17 21:48:04 +0530

    linear search

--- Searching/src/linear_search.java
+++ Searching/src/linear_search.java
@@ -1,2 +1,14 @@
 public class linear_search {
+
+    static int search(int[] arr,int target){
+        for(int i=0;i<arr.length;i++) {
+            if (arr[i] == target)
+                return i;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr={2,6,5,1,-0,3};
+        System.out.println(search(arr,-0));
+    }
 }
