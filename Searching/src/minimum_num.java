c37bd9b229b80fed45855c16c189aca990d18594
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-18 19:00:51 +0530

    linear search get min and sec min

--- Searching/src/minimum_num.java
+++ Searching/src/minimum_num.java
@@ -1,14 +1,20 @@
 public class minimum_num {
     public static void main(String[] args) {
-        int[] nums={5,4,1,3,8,-9,-2};
+        int[] nums={2,4,3,8,-9};
         System.out.println(getMin(nums));
     }
     static int getMin(int [] nums){
         int min=Integer.MAX_VALUE;
-        for(int x:nums)
-            if(x<min)
-                min=x;
+        int min2=nums[1];
+        for(int x:nums) {
+            if (x < min ) {
+                min2 = min;
+                min = x;
+            }
+            else
+                min2=x;
+        }
 
-        return min;
+        return min2;
     }
 }
