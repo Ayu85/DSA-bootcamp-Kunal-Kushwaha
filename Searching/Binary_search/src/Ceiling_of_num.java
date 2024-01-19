738fef9c0ad4912dfed5b3f3290a001c42787274
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-19 15:02:28 +0530

    ceiling of number optimal code using binary search

--- Searching/Binary_search/src/Ceiling_of_num.java
+++ Searching/Binary_search/src/Ceiling_of_num.java
@@ -1,7 +1,7 @@
 public class Ceiling_of_num {
     public static void main(String[] args) {
         int[] arr={2,3,5,6,7,10,14,16,18};
-        System.out.println(getCeilBetter(arr,8));
+        System.out.println(getCeilBest(arr,4));
     }
     static int getCeil(int[] arr,int target){
         int ceil=0;
@@ -20,11 +20,25 @@ public class Ceiling_of_num {
             int mid=(left+right)/2;
             if(target>=arr[mid] && target<arr[mid+1] )
                 return arr[mid+1];
-            else if(target>arr[mid])
+            else if(target>=arr[mid])
                 left=mid+1;
             else
                 right=mid-1;
         }
         return -1;
+    }
+    static int getCeilBest(int[] arr,int target){
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]==target)
+                return mid;
+            else if(target>arr[mid])
+                start=mid+1;
+            else
+                end=mid-1;
+        }
+        return arr[start];
     }
 }
