1a1862693d724a033b0073a5518e4d5e949b227c
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-19 12:00:03 +0530

    binary search

--- Searching/Binary_search/src/BinarySearch.java
+++ Searching/Binary_search/src/BinarySearch.java
@@ -1,2 +1,21 @@
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr={2,4,6,8,10};
+        System.out.println(search(arr,10));
+    }
+    static int search(int[] arr,int target){
+        int left=0;
+        int right= arr.length-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(arr[mid]==target)
+                return mid;
+            else if (arr[mid]>target) {
+                right=mid-1;
+            }
+            else
+                left=mid+1;
+        }
+        return -1;
+    }
 }
