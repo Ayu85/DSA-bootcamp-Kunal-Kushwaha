93d09e7c3f0e3a9dc1abadcb186e3e2ea517c97f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-21 00:32:41 +0530

    optimal approach

--- Searching/Binary_search/src/peak_index_in_mountain_array.java
+++ Searching/Binary_search/src/peak_index_in_mountain_array.java
@@ -1,15 +1,30 @@
 public class peak_index_in_mountain_array {
     public static void main(String[] args) {
-        int[] arr = {8, 10, 5, 12};
-        System.out.println(peakIndexInMountainArray(arr));
+        int[] arr = {8,10,15,18,80,7};
+        System.out.println(getIndexBinarySeacrh(arr));
     }
 
     static int peakIndexInMountainArray(int[] arr) {
         int peak = 0;
         for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] > arr[i + 1] && arr[i]>arr[peak])
+            if (arr[i] > arr[i + 1] && arr[i] > arr[peak])
                 peak = i;
         }
-        return arr[peak] > arr[arr.length - 1] ? peak : arr.length - 1;
+        return peak;
+    }
+
+    static int getIndexBinarySeacrh(int[] arr) {
+        int peak = 0;
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                peak = mid;
+                right=mid-1;
+            } else
+                left = mid + 1;
+        }
+        return peak;
     }
 }
