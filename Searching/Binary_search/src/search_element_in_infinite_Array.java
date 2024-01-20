77db928c0cafc553fce0319da4b1847802c3f24b
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-20 16:15:35 +0530

    optimal force approach

--- Searching/Binary_search/src/search_element_in_infinite_Array.java
+++ Searching/Binary_search/src/search_element_in_infinite_Array.java
@@ -1,7 +1,7 @@
 public class search_element_in_infinite_Array {
     public static void main(String[] args) {
         int[] arr = {2, 4, 5, 6, 8, 9, 10, 12, 14, 15, 18, 19, 22, 23};
-        System.out.println(getIndex(arr,23));
+        System.out.println(getIndex(arr, 23));
 
 //    static int getIndex(int[] arr, int key,int size) {
 //        int start = size - 2;
@@ -29,21 +29,19 @@ public class search_element_in_infinite_Array {
     static int getIndex(int[] arr, int key) {
         int start = 0;
         int end = 1;
-        int p = 1;
-        int ind=search(arr, key, start, end);
-        while(key!=arr[ind]){
-            int newstart=end+1;
-            end = end+(end-start+1)*2;
-            start=newstart;
-            ind=search(arr, key, start, end);
-
+        int ind = search(arr, key, start, end);
+        while (key != arr[ind]) {
+            int newstart = end + 1;
+            end = end + (end - start + 1) * 2;
+            start = newstart;
+            ind = search(arr, key, start, end);
         }
         return ind;
     }
 
     static int search(int[] arr, int key, int start, int end) {
-        int s=start;
-        int e=end;
+        int s = start;
+        int e = end;
         while (s <= e) {
             int mid = (s + e) / 2;
             if (arr[mid] == key)
