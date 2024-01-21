7bcd1a23b2d012bbde246cbbcf1f00ca5cc8cc11
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-21 12:31:28 +0530

    rotate by k

--- Searching/Binary_search/src/rotate_by_k.java
+++ Searching/Binary_search/src/rotate_by_k.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class rotate_by_k {
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,5};
+        int[] arr={1,2,3,4,5,6,7};
         System.out.println("before rotation: ");
         System.out.println(Arrays.toString(arr));
         rotateByK(arr,2);
@@ -14,14 +14,13 @@ public class rotate_by_k {
         int temp[] = new int[k];
 //        storing the k+1 elements in temp array
         int j = 0;
-        for (int i = k + 1; i < arr.length; i++) {
+        for (int i = arr.length - k; i < arr.length; i++) {
             temp[j++] = arr[i];
         }
 //        move the starting elements to the end
-        int m=2;
-        for (int i = k; i >= 0; i--) {
-            arr[m+2] = arr[i];
-            m--;
+
+        for (int i = arr.length-k-1; i >= 0; i--) {
+            arr[i+2] = arr[i];
 
         }
 //        place the temp array elemtns in the starting of array
